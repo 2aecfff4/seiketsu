0045d257b6146dd3130c5a4f100f189438e532b3
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2025-07-02 21:58:57 +0200

    Update 02-install-copr-repos.sh

--- build_files/02-install-copr-repos.sh
+++ build_files/02-install-copr-repos.sh
@@ -20,7 +20,7 @@ dnf5 -y copr enable che/nerd-fonts
 
 if [[ "${BASE_IMAGE_TAG}" =~ main ]]; then
     echo "adding coprs for main image..."
-    dnf5 -y copr enable @kernel-vanilla/stable-rc
+    dnf5 -y copr enable @kernel-vanilla/next
     
     dnf5 -y update --refresh
     sudo dnf upgrade 'kernel*'
