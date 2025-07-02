1b87d8586abacf82424d7b45bcc441bd60efeb1b
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2025-07-03 01:13:52 +0200

    Update 02-install-copr-repos.sh

--- build_files/02-install-copr-repos.sh
+++ build_files/02-install-copr-repos.sh
@@ -25,7 +25,8 @@ if [[ "${BASE_IMAGE_TAG}" =~ main ]]; then
     # dnf5 -y update --refresh
     # sudo dnf upgrade 'kernel*'
     dnf5 -y install kernel-*-6.14.3
-    dnf5 -y remove $(dnf rq --installonly --latest-limit=-1)
+    # dnf5 -y remove $(dnf rq --installonly --latest-limit=-1)
+    dnf -y remove --oldinstallonly
     
     # dnf5 -y copr enable @kernel-vanilla/next
     # dnf5 -y copr enable danayer/mesa-git
