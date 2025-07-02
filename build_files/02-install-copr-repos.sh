0da61dffebbe985e02717527026894a0e852c6a2
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2025-07-02 21:11:49 +0200

    Update 02-install-copr-repos.sh

--- build_files/02-install-copr-repos.sh
+++ build_files/02-install-copr-repos.sh
@@ -21,7 +21,9 @@ dnf5 -y copr enable che/nerd-fonts
 if [[ "${BASE_IMAGE_TAG}" =~ main ]]; then
     echo "adding coprs for main image..."
     dnf5 -y copr enable @kernel-vanilla/mainline
+    
     dnf5 -y update --refresh
+    sudo dnf upgrade 'kernel*'
     
     # dnf5 -y copr enable @kernel-vanilla/next
     # dnf5 -y copr enable danayer/mesa-git
