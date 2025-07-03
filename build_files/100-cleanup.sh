d4850d9bebd0a97cb26317f3ba51ed892c9bc473
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2025-07-03 22:40:47 +0200

    Update 100-cleanup.sh

--- build_files/100-cleanup.sh
+++ build_files/100-cleanup.sh
@@ -40,12 +40,10 @@ fi
 old_kernels=($(dnf repoquery --installonly --latest-limit=-1 -q))
 if [ "${#old_kernels[@]}" -eq 0 ]; then
     echo "No old kernels found"
-    exit 0
 fi
 
 if ! dnf remove "${old_kernels[@]}"; then
     echo "Failed to remove old kernels"
-    exit 1
 fi
 
 # https://bugzilla.redhat.com/show_bug.cgi?id=2185410
