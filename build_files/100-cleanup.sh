66baf451dc35d370ea1695b98d7edf142d3c5f0c
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2025-07-06 20:37:15 +0200

    Update 100-cleanup.sh

--- build_files/100-cleanup.sh
+++ build_files/100-cleanup.sh
@@ -13,7 +13,7 @@ flatpak remote-add --system --if-not-exists flathub https://flathub.org/repo/fla
 systemctl disable flatpak-add-fedora-repos.service
 
 # Disable all COPRs and RPM Fusion Repos and terra
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+# sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 # sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/tailscale.repo
 
 # Disable coprs
