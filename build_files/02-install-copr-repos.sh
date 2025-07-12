e0120aad784cd927681ef59b895c4ffbe64e7120
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2025-07-12 17:09:27 +0200

    Update 02-install-copr-repos.sh

--- build_files/02-install-copr-repos.sh
+++ build_files/02-install-copr-repos.sh
@@ -38,6 +38,11 @@ if [[ "${BASE_IMAGE_TAG}" =~ main ]]; then
     
     # dnf5 -y copr enable @kernel-vanilla/next
     # dnf5 -y copr enable danayer/mesa-git
+
+    dnf5 -y install dnf-plugin-versionlock 
+    dnf5 -y versionlock add kernel-6.14.11-300.fc42.x86_64 
+    dnf5 -y install kernel-6.14.11-300.fc42.x86_64 \
+                    kernel-*-6.14.11-300.fc42.x86_64 
 fi
 
 
